595626379026751e8ffe7bcf9ac045aa055f734f
anmol420 <anmolanand2k15@gmail.com>
2025-08-04 20:56:02 +0530

    Added Database Connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,19 +3,33 @@ package main
 import (
 	"log"
 
+	"github.com/anmol420/Social/internal/db"
 	"github.com/anmol420/Social/internal/env"
 	"github.com/anmol420/Social/internal/store"
 )
 
 func main() {
-	addr := env.GetEnv("ADDR")
-	if addr == "" {
-		log.Fatal("ADDR environment variable is not set")
-	}
+	addr := env.StringGetEnv("ADDR")
+	databaseAddr := env.StringGetEnv("DATABASE_ADDR")
+	maxOpenConns := env.IntegerGetEnv("MAX_OPEN_CONNS")
+	maxIdleConns := env.IntegerGetEnv("MAX_IDLE_CONNS")
+	maxIdleTime := env.StringGetEnv("MAX_IDLE_TIME")
 	cfg := config{
 		addr: addr,
+		db: dbConfig{
+			addr:         databaseAddr,
+			maxOpenConns: maxOpenConns,
+			maxIdleConns: maxIdleConns,
+			maxIdleTime:  maxIdleTime,
+		},
 	}
-	store := store.NewStorage(nil)
+	db, err := db.New(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer db.Close()
+	log.Println("Database Connection Established!")
+	store := store.NewStorage(db)
 	app := &application{
 		config: cfg,
 		store:  store,
