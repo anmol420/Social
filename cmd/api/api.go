1494055cbf72f237830f77690afe957f94095aa2
anmol420 <anmolanand2k15@gmail.com>
2025-08-10 12:19:54 +0530

    Added Create Posts Route

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -39,6 +39,9 @@ func (app *application) mount() http.Handler {
 
 	r.Route("/v1", func(r chi.Router) {
 		r.Get("/health", app.healthCheckHandler)
+		r.Route("/posts", func(r chi.Router) {
+			r.Post("/create", app.createPostsHandler)
+		})
 	})
 
 	return r
