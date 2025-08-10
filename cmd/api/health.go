64069c18bf9e5925eb924f63ca51351ea2048cf0
anmol420 <anmolanand2k15@gmail.com>
2025-08-10 11:45:14 +0530

    Added Json Marshalling

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -1,7 +1,15 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Ok!"))
+	data := map[string]any{
+		"status": http.StatusOK,
+		"data":   "Ok!",
+	}
+	if err := writeJSON(w, http.StatusOK, data); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, "Something Went Wrong!")
+	}
 }
