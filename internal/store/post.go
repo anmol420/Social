7511b7b1f755967d2328ab3de4a4e5565466a1e5
anmol420 <anmolanand2k15@gmail.com>
2025-08-10 13:05:45 +0530

    Added Function For Fetching Post By ID

--- internal/store/post.go
+++ internal/store/post.go
@@ -3,6 +3,7 @@ package store
 import (
 	"context"
 	"database/sql"
+	"errors"
 
 	"github.com/lib/pq"
 )
@@ -42,4 +43,31 @@ func (s *PostStorage) Create(ctx context.Context, post *Post) error {
 		return err
 	}
 	return nil
+}
+
+func (s *PostStorage) GetByID(ctx context.Context, id int64) (*Post, error) {
+	query := `
+		SELECT id, title, content, tags, user_id, created_at, updated_at
+		FROM posts 
+		WHERE id = $1
+	`
+	var post Post
+	err := s.db.QueryRowContext(ctx, query, id).Scan(
+		&post.ID,
+		&post.Title,
+		&post.Content,
+		pq.Array(&post.Tags),
+		&post.UserID,
+		&post.CreatedAt,
+		&post.UpdatedAt,
+	)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrNotFound
+		default:
+			return nil, err
+		}
+	}
+	return &post, nil
 }
